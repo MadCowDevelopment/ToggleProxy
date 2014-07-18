d0d320c06999ab02b3f84a7fc5e2d72228301e6c
Mihi <devdumbblind@googlemail.com>
2014-07-18 08:33:26 +0800

    added setup project

---
+++ ToggleProxy/InstallerClass.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+
+namespace ToggleProxy
+{
+    [RunInstaller(true)]
+    public class InstallerClass : Installer
+    {
+        public InstallerClass()
+        {
+            // Attach the 'Committed' event.
+            Committed += MyInstaller_Committed;
+            // Attach the 'Committing' event.
+            Committing += MyInstaller_Committing;
+        }
+
+        // Event handler for 'Committing' event.
+        private void MyInstaller_Committing(object sender, InstallEventArgs e)
+        {
+            //Console.WriteLine("");
+            //Console.WriteLine("Committing Event occurred.");
+            //Console.WriteLine("");
+        }
+
+        // Event handler for 'Committed' event.
+        private void MyInstaller_Committed(object sender, InstallEventArgs e)
+        {
+            try
+            {
+                Directory.SetCurrentDirectory(Path.GetDirectoryName
+                (Assembly.GetExecutingAssembly().Location));
+                Process.Start(Path.GetDirectoryName(
+                    Assembly.GetExecutingAssembly().Location) + "\\ToggleProxy.exe");
+            }
+            catch
+            {
+                // Do nothing... 
+            }
+        }
+
+        // Override the 'Install' method.
+        public override void Install(IDictionary savedState)
+        {
+            base.Install(savedState);
+        }
+    }
+}
